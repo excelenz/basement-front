2f6cadc125305894cac92494971576e35cabd2d8
steppenwolf <margolin.dm@gmail.com>
2019-08-18 01:19:56 +0300

    ff

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,28 +7,60 @@ import person_data from "../db_data/id1/id1.json";
 
 
 export default class Profile extends Component {
-
+      addQuestion = e => {
+        e.preventDefault()
+        alert("new one")
+      }
 
     render() {
           var data = person_data[0];
           return (
-            <div className="self-profile">
-               {this.props.match.url}
-
-              <Thumb image_url={data.avatar} />
-              <div className="texts-box">
-                <div className="card-title self-profile-paragraph">
-                    {! data.first_name ? "" : "Имя: "+ data.first_name} {! data.last_name ? "" : "Фамилия "+ data.last_name}
-                    <br />
-                    страна: { data.country}
-                    <br />
-                    {! data.placeofbirth? "" : "место рождения: "+ data.placeofbirth}
-                    <br />
-                    {! data.dateofbirth ? "" : "дата рождения: "+  data.dateofbirth}
-                </div>
-                <p className="card-msg">{ data.description}</p>
-              </div>
-            </div>
+                <div className="self-profile">
+                    <Thumb image_url={data.avatar} />
+                    <div className="texts-box">
+                    <div className="card-title self-profile-paragraph">
+                        {! data.first_name ? "" : "Имя: "+ data.first_name} {! data.last_name ? "" : "Фамилия "+ data.last_name}
+                        <br />
+                        страна: { data.country}
+                        <br />
+                        {! data.placeofbirth? "" : "место рождения: "+ data.placeofbirth}
+                        <br />
+                        {! data.dateofbirth ? "" : "дата рождения: "+  data.dateofbirth}
+                    </div>
+                    <p className="card-msg">{ data.description}</p>
+                    </div>
+                    <div className="self-profile-section" >
+                        <div className="self-profile-block">
+                              <div className="texts-box">
+                              <div className="self-profile-block_title">Персональная информация</div>
+                                <div className="card-title self-profile-paragraph">
+                                    {! data.first_name ? "" : "Имя: "+ data.first_name} {! data.last_name ? "" : "Фамилия "+ data.last_name}
+                                    <br />
+                                    страна: { data.country}
+                                    <br />
+                                    {! data.placeofbirth? "" : "место рождения: "+ data.placeofbirth}
+                                    <br />
+                                    {! data.dateofbirth ? "" : "дата рождения: "+  data.dateofbirth}
+                                </div>
+                              </div>
+                        </div>
+                        <div className="self-profile-block">
+                              <div className="texts-box">
+                              <div className="self-profile-block_title">Образование</div>
+                                <div className="card-title self-profile-paragraph">
+                                    {! data.first_name ? "" : "Имя: "+ data.first_name} {! data.last_name ? "" : "Фамилия "+ data.last_name}
+                                    <br />
+                                    страна: { data.country}
+                                    <br />
+                                    {! data.placeofbirth? "" : "место рождения: "+ data.placeofbirth}
+                                    <br />
+                                    {! data.dateofbirth ? "" : "дата рождения: "+  data.dateofbirth}
+                                </div>
+                              </div>
+                        </div>
+                    </div>
+                    <button onClick={this.addQuestion}>add new block</button>
+                 </div>
           );
     }
 }
