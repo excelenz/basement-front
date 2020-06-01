e0ebac2d58c412041250f570b6c54ca403f8ab08
excelenz <margolin@visart.biz>
2020-06-01 16:22:29 +0200

    pushing to master using bush file

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 from resources.Entry import EntryResource
 from resources.Organisation import OrganisationResource
+from resources.Hello import Hello
 
 
 api_bp = Blueprint('api', __name__)
@@ -11,3 +12,4 @@ api = Api(api_bp)
 # Route
 api.add_resource(EntryResource, '/Entry')
 api.add_resource(OrganisationResource, '/Org')
+api.add_resource(Hello, '/Hello')
--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 
+app = Flask(__name__)
 
 def create_app(config_filename):
-    app = Flask(__name__)
+
     app.config.from_object(config_filename)
     
     from app import api_bp
@@ -11,9 +12,12 @@ def create_app(config_filename):
 
     from Model import db
     db.init_app(app)
-
     return app
 
+@app.route("/home")
+def welcome():
+    print("ASdasdasdas")
+    return "ASdasdasdas"
 
 if __name__ == "__main__":
     app = create_app("config")
